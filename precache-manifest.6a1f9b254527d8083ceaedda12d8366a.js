fa79856c10e7c4b1c14027975ae8b9848b611f5c
Tao Gao <tao.gao@oracle.com>
2019-10-07 22:25:46 -0400

    Updates

---
+++ precache-manifest.6a1f9b254527d8083ceaedda12d8366a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b935c7fdf18dbb63da500697679261",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6a03e3f83b71f56bbcbd",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6a03e3f83b71f56bbcbd",
+    "url": "/contactmanager/static/js/2.9f2c07c9.chunk.js"
+  },
+  {
+    "revision": "3cbeb99a7d70a7082b40",
+    "url": "/contactmanager/static/js/main.81608167.chunk.js"
+  },
+  {
+    "revision": "485c57d5d1665b41ffe4",
+    "url": "/contactmanager/static/js/runtime-main.529d79c8.js"
+  }
+]);
